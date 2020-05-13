771eed6c7ac4ac0fe08e128e85514056afa24446
Natalia Munina <nataly.munina@gmail.com>
2020-05-13 12:11:54 +0200

    added Yatzy.java to describe the game added YatzyApplication.java to run the game

---
+++ src/main/java/common/Yatzy.java
@@ -0,0 +1,12 @@
+package common;
+
+public class Yatzy {
+
+	//how does the game look like
+	//user name(-s)
+	//user score
+	//who is the winner
+
+
+
+}
---
+++ src/main/java/common/YatzyApplication.java
@@ -0,0 +1,26 @@
+package common;
+
+public class YatzyApplication {
+
+	public static void main(String[] args) {
+		boolean doYouWantToPlay = true;
+
+		//TODO: How do we play the game. Instructions
+
+		//start the game
+		while (doYouWantToPlay) {
+			//keep playing
+
+			System.out.println("All right. Let's play Yatzy!");
+
+			//enter number of players
+			//create new game instance
+			Yatzy game = new Yatzy();
+
+			//TODO: play until the game is over
+
+
+
+		}
+	}
+}
