b9de67ed5b2b31ef3163a9a62e015e53b907787b
Natalia Munina <nataly.munina@gmail.com>
2020-05-14 14:58:49 +0200

    solving commit problem

---
+++ src/main/java/common/Player.java
@@ -0,0 +1,24 @@
+package common;
+
+public class Player {
+    String name = "OnePlayerGameDummy";
+    boolean winner;
+
+    //Player player = new Player();
+
+    public static void increaseScore(int points) {
+    }
+
+    public static String getScore() {
+        return "current score";
+    }
+
+    public static int[] choise() {
+        // wich dice you want to reroll
+        // numbers in the array
+
+        int[] firstRoll = new int[5];
+
+        return firstRoll;
+    }
+}
