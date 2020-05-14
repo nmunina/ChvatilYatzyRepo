f935c6a8c69fc78c30ed8df5c8044f7dad192161
alexanderbram <alexnader.bram@utb.ecutbildning.se>
2020-05-14 15:52:15 +0200

    createPlayers reworked

--- src/main/java/common/YatzyOperations.java
+++ src/main/java/common/YatzyOperations.java
@@ -12,30 +12,40 @@ public class YatzyOperations {
 		return newRoll;
 	}
 	
-	public static Player[] createPlayers() {
-
-
-		//enter number of players
-		System.out.println("Enter amount of players (1 - 4): ");
-		int numPlayers = scanner.nextInt();
-		Player[] players = new Player[numPlayers];
-
-		if ((numPlayers >= 1) || (numPlayers <= 4)) {
-
-			for (int i = 0; i < players.length; i++) {
-				Player player = new Player();
-
-				System.out.println("Player " + (i+1) + " enter your name :"); // "Player " + (i+1) + " enter your name :" (?)
-				String name = scanner.nextLine();
-				player.name = name;
-				System.out.println("why do not ask?");
-
-			}
-
-		scanner.close();
-		} //TODO: else {error message}
+//	public static Player[] createPlayers() {
+//
+//
+//		//enter number of players
+//		System.out.println("Enter amount of players (1 - 4): ");
+//		int numPlayers = scanner.nextInt();
+//		Player[] players = new Player[numPlayers];
+//
+//		if ((numPlayers >= 1) || (numPlayers <= 4)) {
+//
+//			for (int i = 0; i < players.length; i++) {
+//				Player player = new Player();
+//
+//				System.out.println("Player " + (i+1) + " enter your name :"); // "Player " + (i+1) + " enter your name :" (?)
+//				String name = scanner.nextLine();
+//				player.name = name;
+//				System.out.println("why do not ask?");
+//
+//			}
+//
+//		scanner.close();
+//		} //TODO: else {error message}
+//		
+//		return players;
+//	}
+	public String[] createPlayers(int amount) {
+		String[] players = new String[amount];
+		String tempName;
+		for (int i = 0; i < players.length; i++) {
+			System.out.println("Player " + (i + 1) + " enter your name: ");
+			tempName = scanner.nextLine();
+			players[i] = tempName; 
+		}
 		
 		return players;
 	}
-	
 }
