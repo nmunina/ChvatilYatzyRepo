832ccb30782e58c053fa6e2a8c7062a4f3f849b0
alexanderbram <alexnader.bram@utb.ecutbildning.se>
2020-05-18 15:19:52 +0200

    Removal of unnecessary snippets

--- src/main/java/common/DiceRoll.java
+++ src/main/java/common/DiceRoll.java
@@ -3,19 +3,10 @@ package common;
 import java.util.Random;
 
 public class DiceRoll {
-	//TODO: implement this for 5 dice
-	private static Random rand = new Random();
 	
 	private static int value;
 	private static int roundPoints = 0;
-
-	//is it constructor? Could you please comment what is this doing and why. Couldnot get
-	//what's the differemce with roll()? Isn't roll() enough?
-//	public DiceRoll() {
-//	    value = 0;
-//	    rand = new Random();
-//	}
-	 
+	
 	public static int roll() {
 		value = (int)(Math.random() * 6) + 1;
 		return value;
